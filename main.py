a91029e941b74e16fc72a4e7012435badb335a34
Isaac Kapeel <ikapeel@gmail.com>
2025-07-06 03:50:01 -0500

    Started searching the flight offers data

--- main.py
+++ main.py
@@ -1,22 +1,49 @@
-from dotenv import load_dotenv
 import os
 import requests
 import json
+import config
 
-load_dotenv()   # Loads variables from .env into environment
 
-api_key = os.getenv("API_KEY")  # API key stored in local .env file
-print(f"Your API key is: {api_key}")
+# API querystring that imports the parameters set in the config.py file
+# -------- IMPORTANT ---------
+# If you left any of the optional parameters in config.py blank such as "children", or if it has a value but you want to omit it
+# then you must comment it out of the querystring dict below.
+querystring = {
+    "fromId": config.FROM_ID,   # REQUIRED PARAMETER
+    "toId": config.TO_ID,   # REQUIRED PARAMETER
+    "departDate": config.DEPART_DATE,   # REQUIRED PARAMETER
+    "returnDate": config.RETURN_DATE,
+    "stops": config.STOPS,
+    "pageNo": config.PAGE_NO,
+    "adults": config.ADULTS,
+    "children": config.CHILDREN,
+    "sort": config.SORT,
+    "cabinClass": config.CABIN_CLASS,
+    "currency_code": config.CURRENCY_CODE
+}
 
 # Mock data used to test features before implementing actual API calls with a key
 # Response is the JSON given as example response on rapidapi
-def testAPI():
+def mockAPI():
     with open("mock_flight_response.json", "r") as f:
         data = json.load(f)
     
     return data["data"]["flightOffers"]
 
+def searchFlightOffers(data):
+    segment = 0
+    for flight in data:
+        #for segments in flight["segments"]:
+            #for legs in segments["legs"]:
+                #pass
+
+        print(f"Flight: {segment}; Total Price: {flight["priceBreakdown"]["total"]["units"]} {config.CURRENCY_CODE}")
+        print()
+        segment += 1
+
 
 if __name__ == "__main__":
-    mock_data = testAPI()
+    mock_data = mockAPI()
     #print(mock_data)
+    #print(querystring)
+    searchFlightOffers(mock_data)
